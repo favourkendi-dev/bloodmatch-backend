f2a79dd27dcbd2613cfbdec8dcfc159d56087646
favourkendi-dev <favour.kendi@student.moringaschool.com>
2026-07-20 22:24:19 +0300

    Add DonorProfile model with blood type, location and availability

--- donors/admin.py
+++ donors/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import DonorProfile
 
-# Register your models here.
+
+@admin.register(DonorProfile)
+class DonorProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'blood_type', 'city', 'is_available', 'last_donation_date')
+    list_filter = ('blood_type', 'is_available', 'city')
+    search_fields = ('user__username', 'user__email', 'city')
